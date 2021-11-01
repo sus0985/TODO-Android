cfd3e952434496ee1f8fc1fb291881f76ba505d0
신재혁 <shs0985@gmail.com>
2021-11-02 01:05:28 +0900

    [Feat] BindingAdapter parameter type 변경

--- app/src/main/java/com/example/todo/common/BindingAdapters.kt
+++ app/src/main/java/com/example/todo/common/BindingAdapters.kt
@@ -6,6 +6,6 @@ import com.example.todo.model.Tag
 import com.example.todo.ui.home.HomeTagAdapter
 
 @BindingAdapter("bind:setHomeTagItems")
-fun setHomeTagItems(listAdapter: RecyclerView, list: List<Tag>) {
+fun setHomeTagItems(listAdapter: RecyclerView, list: List<Tag>?) {
     (listAdapter.adapter as HomeTagAdapter).submitList(list)
 }
