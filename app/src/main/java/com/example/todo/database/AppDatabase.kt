051abb16b8d9278cf67ee4e4b337d0e2dcf785f1
신재혁 <shs0985@gmail.com>
2021-11-07 11:04:00 +0900

    [Feat] Category 필드에 색상, Task 갯수 속성 추가 및 초기 데이터베이스 변경

--- app/src/main/java/com/example/todo/database/AppDatabase.kt
+++ app/src/main/java/com/example/todo/database/AppDatabase.kt
@@ -2,9 +2,11 @@ package com.example.todo.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.todo.model.Category
 
 @Database(entities = [Category::class], version = 1)
+@TypeConverters(ColorConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
 }
--- app/src/main/java/com/example/todo/model/Category.kt
+++ app/src/main/java/com/example/todo/model/Category.kt
@@ -7,5 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "CATEGORY")
 data class Category(
     @PrimaryKey val id: Int,
-    @ColumnInfo(name = "text") val text: String
+    @ColumnInfo(name = "text") val text: String,
+    @ColumnInfo(name = "color") val color: ColorCode,
+    @ColumnInfo(name = "task_count") val taskCount: Int
 )
